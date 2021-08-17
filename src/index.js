8bc11cea2b196e387a732867fcebfad8a46bb813
HENRY-KC <henrykc24@yahoo.com>
2021-08-17 20:37:37 +0100

    Add remove item, check item

--- src/index.js
+++ src/index.js
@@ -1,14 +1,97 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+document.querySelector('.footer-text').innerHTML = `&copy; ${new Date().getFullYear()} Henry-Kc, built with 💕 from me`;
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const generateToDoRows = (text, index) => {
+  const div = document.createElement('div');
+  div.classList.add('to-do-row', 'custom-row');
 
-  return element;
+  const div2 = document.createElement('div');
+  div2.classList.add('two');
+
+  const input = document.createElement('input');
+  input.classList.add('checkbox');
+  input.type = 'checkbox';
+  input.addEventListener('click', () => {
+    console.log(index);
+    console.log('iiiii', input.checked);
+    if(input.checked) {
+      toBeDeleted.push(index);
+    } else {
+      toBeDeleted.splice(toBeDeleted.indexOf(index), 1);
+    }
+    console.log(toBeDeleted);
+    // return remove(index);
+  })
+  div2.appendChild(input);
+
+  const input2 = document.createElement('input');
+  input2.classList.add('to-do');
+  input2.type = 'text';
+  input2.value = text;
+  div2.appendChild(input2);
+
+  div.appendChild(div2);
+  
+  const i = document.createElement('i');
+  i.classList.add('fas', 'fa-ellipsis-v');
+  div.appendChild(i);
+
+  document.querySelector('.to-do-list').appendChild(div);
+  return true;
 }
 
-// document.body.appendChild(component());
+let tasks = [
+  {
+    description: 'Example text',
+    completed: true,
+    index: 0,
+  },
+];
+
+const toBeDeleted = [];
+
+const remove = () => {
+  tasks = tasks.filter((task) => !toBeDeleted.includes(task.index));
+  showToDo(tasks);
+}
+
+document.querySelector('.clear-text').addEventListener('click', remove);
+
+const addToDo = () => {
+  const description = document.querySelector('.input').value;
+  if (!description) return;
+  const indexArray = tasks.map(({index}) => index);
+  const sortedIndeces = indexArray.sort((a, b) => a - b);
+  const highestIndex = sortedIndeces[sortedIndeces.length - 1];
+  const data = {
+    description,
+    index: highestIndex + 1,
+    completed: false,
+  }
+  tasks.push(data);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  showToDo(tasks);
+  document.querySelector('.input').value = '';
+};
+
+document.querySelector('.return').addEventListener('click', () => {
+  return addToDo();
+});
+
+document.querySelector('.input').addEventListener('keypress', (e) => {
+ if (e.key === 'Enter') {
+   return addToDo();
+ }
+});
+
+if(localStorage.getItem('tasks') !== null) {
+  tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+const showToDo = (tasks) => {
+  document.querySelector('.to-do-list').innerHTML = ''; 
+  tasks.forEach((task) => generateToDoRows(task.description, task.index));
+};
+console.log(tasks);
+showToDo(tasks);
