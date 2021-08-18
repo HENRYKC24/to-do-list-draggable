26ea6f3bc48c6761d04cd6b8bb7ac607ece05268
HENRY-KC <henrykc24@yahoo.com>
2021-08-18 09:42:27 +0100

    Remove unwanted variables

--- src/completedToDo.js
+++ src/completedToDo.js
@@ -1,5 +1,12 @@
-const updateCompleted = (input, task) => {
+const updateCompleted = (task) => {
   task.completed = !task.completed;
 };
 
+const addEventListenerToLinks = (addToDo, remove, refresh) => {
+  document.querySelector('.item:nth-child(1)').addEventListener('click', addToDo);
+  document.querySelector('.item:nth-child(2)').addEventListener('click', remove);
+  document.querySelector('.item:nth-child(3)').addEventListener('click', refresh);
+};
+
 export default updateCompleted;
+export { addEventListenerToLinks };
