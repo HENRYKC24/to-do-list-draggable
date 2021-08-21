2dfce8b25ea119a7b447e174f3a03d4910f81b21
HENRY-KC <henrykc24@yahoo.com>
2021-08-21 22:06:00 +0100

    Serialize task object index property and add element reordering persistence logic

--- src/manipulateToDo.js
+++ src/manipulateToDo.js
@@ -1,5 +1,12 @@
 let dragStore = null;
-const onDragOver = (ev) => {
+const reorderIndices = (tasks) => {
+  let index = 1;
+  tasks.forEach((task) => {
+    task.index = index;
+    index += 1;
+  });
+};
+const drop = (ev) => {
   ev.preventDefault();
   const elUnder = ev.target;
   const pElement = elUnder.parentElement;
@@ -7,30 +14,42 @@ const onDragOver = (ev) => {
   if (parentName === 'DIV') {
     const actualDivClass = ev.target.classList[0];
     if (actualDivClass === 'to-do-row') {
-      elUnder.parentElement.insertBefore(dragStore, elUnder);
+      elUnder.parentElement.insertBefore(dragStore, elUnder.nextElementSibling);
     } else if (actualDivClass === 'to-do') {
       elUnder.parentElement.parentElement.parentElement.insertBefore(
         dragStore,
-        elUnder.parentElement.parentElement,
+        elUnder.parentElement.parentElement.nextElementSibling,
       );
     } else if (actualDivClass === 'fas') {
       elUnder.parentElement.parentElement.insertBefore(
         dragStore,
-        elUnder.parentElement,
+        elUnder.parentElement.nextElementSibling,
       );
     } else if (actualDivClass === 'checkbox') {
       elUnder.parentElement.parentElement.parentElement.insertBefore(
         dragStore,
-        elUnder.parentElement.parentElement,
+        elUnder.parentElement.parentElement.nextElementSibling,
       );
     }
   }
 };
 
-const dragEnd = () => {
+const dragEnd = (showToDo) => {
   setTimeout(() => {
     dragStore.style.backgroundColor = '#fff';
   }, 1000);
+
+  const allToDoElements = Array.from(document.querySelectorAll('.to-do-row'));
+  const ids = allToDoElements.map((el) => Number(el.id));
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  const mockTasks = [];
+  ids.forEach((id) => {
+    mockTasks.push(tasks.filter((task) => task.index === id)[0]);
+  });
+  tasks = mockTasks;
+  reorderIndices(tasks);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  showToDo(JSON.parse(localStorage.getItem('tasks')));
 };
 
 const drag = (ev) => {
@@ -46,6 +65,7 @@ const dragStart = (e) => {
 const removeOne = (task, tasks, showToDo) => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
   tasks.splice(tasks.indexOf(task), 1);
+  reorderIndices(tasks);
   // eslint-disable-next-line no-use-before-define
   showToDo(tasks);
 };
@@ -53,6 +73,7 @@ const removeOne = (task, tasks, showToDo) => {
 const removeSelected = (tasks, showToDo) => {
   tasks = JSON.parse(localStorage.getItem('tasks'));
   tasks = tasks.filter((task) => !task.completed);
+  reorderIndices(tasks);
   localStorage.setItem('tasks', JSON.stringify(tasks));
   showToDo(tasks);
   return true;
@@ -65,15 +86,10 @@ const addToDo = (tasks, showToDo, goToInput) => {
     goToInput();
     return false;
   }
-  const indexArray = tasks.map(({ index }) => index);
-  const sortedIndeces = indexArray.sort((a, b) => a - b);
-  let highestIndex = sortedIndeces[sortedIndeces.length - 1];
-  if (Number.isNaN(highestIndex) || !highestIndex) {
-    highestIndex = 0;
-  }
+
   const data = {
     description,
-    index: highestIndex + 1,
+    index: tasks.length + 1,
     completed: false,
   };
   tasks.push(data);
@@ -85,5 +101,11 @@ const addToDo = (tasks, showToDo, goToInput) => {
 };
 
 export {
-  onDragOver, dragEnd, drag, dragStart, removeOne, removeSelected, addToDo,
+  drop,
+  dragEnd,
+  drag,
+  dragStart,
+  removeOne,
+  removeSelected,
+  addToDo,
 };
